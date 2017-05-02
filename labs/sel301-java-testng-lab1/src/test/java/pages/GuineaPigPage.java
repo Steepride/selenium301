57f95c0f8f5cd5bbb6b3f75da7d3839d6e03e7d1
jtack4970 <jtack4970@gmail.com>
2017-05-02 11:15:08 -0700

    added sel libs to import

--- labs/sel301-java-testng-lab1/src/test/java/pages/GuineaPigPage.java
+++ labs/sel301-java-testng-lab1/src/test/java/pages/GuineaPigPage.java
@@ -1,18 +1,20 @@
 package pages;
 
-import static org.testng.AssertJUnit.assertEquals;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.BrowserType;
 
 
 
 public class GuineaPigPage extends PageBase{
-
+	protected WebDriver driver;
+	protected BrowserType browser;
+	
 	public GuineaPigPage(WebDriver driver) {
-		super(driver);
+		this.driver = driver;
 	}
 
 	public void getPage() {
